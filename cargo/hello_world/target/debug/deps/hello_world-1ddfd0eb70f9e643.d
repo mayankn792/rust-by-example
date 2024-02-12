4ab123dc980299d363bdd072b200a0fa23583787
Mayank Negi <mayankn792@gmail.com>
2024-02-12 23:29:41 +0530

    first cargo

---
+++ cargo/hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643.d
@@ -0,0 +1,5 @@
+/Users/mayank/Desktop/hands-on/node/rust by example/cargo/hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643: src/main.rs
+
+/Users/mayank/Desktop/hands-on/node/rust by example/cargo/hello_world/target/debug/deps/hello_world-1ddfd0eb70f9e643.d: src/main.rs
+
+src/main.rs:
---
+++ cargo/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/mayank/Desktop/hands-on/node/rust\ by\ example/cargo/hello_world/target/debug/hello_world: /Users/mayank/Desktop/hands-on/node/rust\ by\ example/cargo/hello_world/src/main.rs
